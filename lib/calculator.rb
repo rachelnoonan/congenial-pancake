9da3b4e51cd3d7849265c6a0db656fae5b36176a
David Power <david.power@wpengine.com>
2017-08-19 21:49:55 +0100

    expect expects a block, not a regular input, as in {} and not ()

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,13 +1,12 @@
-require 'pry'
-# no main class for loading so require is at this level
+# Class: Calculator
+# Methods: Add
+# Takes two values and adds them together
 class Calculator
-
-  def self.add(input1, input2)
+  def add(input1, input2)
     if input1.is_a?(Integer) && input2.is_a?(Integer)
-    input1 + input2
+      input1 + input2
     else
       raise ArgumentError, "Expected integers what i received was #{input1} : #{input2}"
-      end
+    end
   end
-
 end
