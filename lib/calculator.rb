1e32a7a4ad9dfa08d119f1a670e2178d4345fefc
Rachel Noonan <rachel.noonan@gmail.com>
2017-09-18 14:30:45 +0100

    updates for negative test cases

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -3,10 +3,15 @@
 # Takes two values and adds them together
 class Calculator
   def add(input1, input2)
-    if input1.is_a?(Integer) && input2.is_a?(Integer)
-      input1 + input2
-    else
-      raise ArgumentError, "Expected integers what i received was #{input1} : #{input2}"
+    case
+      when input1.nil? || input2.nil?
+        raise TypeError, 'nill input could not add please review'
+      when input1.is_a?(String) || input2.is_a?(String)
+        raise TypeError, "Error #{input1} : #{input2} were not valid inputs could not add please review"
+      when input1.is_a?(Boolean) || input2.is_a?(Boolean)
+        raise TypeError, "Error #{input1} : #{input2} were not valid inputs could not add please review"
+      else
+        input1 + input2
     end
   end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -8,19 +8,19 @@ RSpec.describe Calculator do
         expect(calc.add(1, 3)).to eq(4)
       end
       it 'given two numbers 0,3 return 3' do
-        expect(calc.add(0,3)).to eq(3)
+        expect(calc.add(0, 3)).to eq(3)
       end
       it 'given two numbers 0,0 return 0' do
-        expect(calc.add(0,0)).to eq(0)
+        expect(calc.add(0, 0)).to eq(0)
       end
       it 'given two numbers -5,0 return -5' do
-        expect(calc.add(-5,0)).to eq(-5)
+        expect(calc.add(-5, 0)).to eq(-5)
       end
       it 'given two numbers -1,-3 return 4' do
-        expect(calc.add(-1,-3)).to eq(4)
+        expect(calc.add(-1, -3)).to eq(4)
       end
       it 'given float and integer 10.58, 11 return 21.58' do
-        expect(calc.add(10.58,11)).to eq(21.58)
+        expect(calc.add(10.58, 11)).to eq(21.58)
       end
       # Review context include more scenarios for tests
     end
@@ -28,14 +28,13 @@ RSpec.describe Calculator do
       it 'given string:bob and int:0 input expect exception of type ArgumentError' do
         expect { calc.add('bob', 0) }.to raise_error(ArgumentError)
       end
-
-      it 'given '' and int:20 input expect exception of type ArgumentError' do
-        expect(calc.add('',20)).to raise_error(ArgumentError)
+      it 'given " " and int:20 input expect exception of type ArgumentError' do
+        expect { calc.add('', 20) }.to raise_error(ArgumentError)
       end
       it 'given null and int:20 input expect exception of type ArgumentError' do
-        expect(calc.add(null,20)).to raise_error(ArgumentError)
-      end
+        expect { calc.add(null, 20) }.to raise_error(ArgumentError)
       end
     end
-  # Review context include more scenarios for failing tests
   end
+  # Review context include more scenarios for failing tests
+end
